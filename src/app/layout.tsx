a929881931ba34c3c923dff8c17ec7241d598419
matheusgondra <matheusgondra951@gmail.com>
2025-05-21 13:12:20 -0300

    chore: import fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"]
+const inter = Inter({
+	variable: "--font-primary",
+	subsets: ["latin"],
+	weight: ["400"]
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"]
+const roboto = Roboto({
+	variable: "--font-secondary",
+	subsets: ["latin"],
+	weight: ["400"]
 });
 
 export const metadata: Metadata = {
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+			<body className={`${roboto.variable} ${inter.variable}`}>{children}</body>
 		</html>
 	);
 }
