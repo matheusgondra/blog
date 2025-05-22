5a30db205e3088451754c74374769c2470d1fbbd
matheusgondra <matheusgondra951@gmail.com>
2025-05-22 07:40:43 -0300

    test: ensure Header render a avartar image

--- src/components/Header/header.spec.tsx
+++ src/components/Header/header.spec.tsx
@@ -1,6 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import type { ImgHTMLAttributes } from "react";
 import { Header } from ".";
+
+vi.mock("next/image", () => ({
+	_esModule: true,
+	default: (props: ImgHTMLAttributes<HTMLImageElement>) => {
+		return <img {...props} alt={props.alt} />;
+	}
+}));
 
 describe("Header", () => {
 	it("Should render a header element", () => {
@@ -9,5 +16,13 @@ describe("Header", () => {
 		const headerElement = screen.getByRole("banner");
 
 		expect(headerElement).toBeDefined();
+	});
+
+	it("Should render a avatar image", () => {
+		render(<Header />);
+
+		const avatarImage = screen.getByAltText("imagem de perfil de Matheus de Gondra");
+
+		expect(avatarImage).toBeInTheDocument();
 	});
 });
