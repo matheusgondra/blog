89da6449523453c22fa6ed8e6f2b5e98b0896e07
matheusgondra <matheusgondra951@gmail.com>
2025-05-22 07:47:59 -0300

    feat: ensure Header render a title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,8 @@ export function Header() {
 	return (
 		<header>
 			<Image src="/avatar.jpg" alt="imagem de perfil de Matheus de Gondra" width={42} height={42} />
+
+			<h1>Gondra Blog</h1>
 		</header>
 	);
 }
