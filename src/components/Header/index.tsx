7a1021579dfbc85545ca62101a149b7752dbc8af
matheusgondra <matheusgondra951@gmail.com>
2025-05-22 07:56:29 -0300

    feat: ensure Header render a navigation list with two links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,17 @@ export function Header() {
 			<Image src="/avatar.jpg" alt="imagem de perfil de Matheus de Gondra" width={42} height={42} />
 
 			<h1>Gondra Blog</h1>
+
+			<nav>
+				<ul>
+					<li>
+						<a href="https://www.linkedin.com/in/matheusgondra/">Matheus Gondra</a>
+					</li>
+					<li>
+						<a href="https://github.com/matheusgondra">matheusgondra</a>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 }
