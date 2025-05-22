d4aea0da41b3c19be61628f834828968d4fec827
matheusgondra <matheusgondra951@gmail.com>
2025-05-22 07:40:31 -0300

    feat: ensure Header render a avartar image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
+
 export function Header() {
-	return <header />;
+	return (
+		<header>
+			<Image src="/avatar.jpg" alt="imagem de perfil de Matheus de Gondra" width={42} height={42} />
+		</header>
+	);
 }
