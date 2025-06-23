0b48af98c685d8122188451c172f72d5847039f0
matheusgondra <matheusgondra951@gmail.com>
2025-06-23 07:55:37 -0300

    styles: header mobile style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import { Header } from "@/components/Header";
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Hello</h1>
+		<>
 			<Header />
-		</div>
+		</>
 	);
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,36 @@
 import Image from "next/image";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import styles from "./styles.module.css";
 
 export function Header() {
 	return (
-		<header>
-			<Image src="/avatar.jpg" alt="imagem de perfil de Matheus de Gondra" width={42} height={42} />
+		<header className={styles.header}>
+			<Image
+				className={styles.avatar}
+				src="/avatar.jpg"
+				alt="imagem de perfil de Matheus de Gondra"
+				width={42}
+				height={42}
+			/>
 
-			<h1>Gondra Blog</h1>
+			<div>
+				<h1 className={styles.title}>Gondra Blog</h1>
 
-			<nav>
-				<ul>
-					<li>
-						<a target="__blank" href="https://www.linkedin.com/in/matheusgondra/">
-							<FaGithub className="icon" /> Matheus Gondra
-						</a>
-					</li>
-					<li>
-						<a target="__blank" href="https://github.com/matheusgondra">
-							<FaLinkedin className="icon" /> matheusgondra
-						</a>
-					</li>
-				</ul>
-			</nav>
+				<nav>
+					<ul className={styles.list_links}>
+						<li>
+							<a className={styles.link} target="__blank" href="https://www.linkedin.com/in/matheusgondra/">
+								<FaGithub className="icon" /> Matheus Gondra
+							</a>
+						</li>
+						<li>
+							<a className={styles.link} target="__blank" href="https://github.com/matheusgondra">
+								<FaLinkedin className="icon" /> matheusgondra
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 }
