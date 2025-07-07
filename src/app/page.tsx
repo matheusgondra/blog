a4f4a68753be5edde841f40e00210e44ad327adb
matheusgondra <matheusgondra951@gmail.com>
2025-07-07 09:10:31 -0300

    styles: style Post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,16 @@ export default function Home() {
 
 				<div
 					style={{
-						marginTop: "10px",
-						display: "grid",
-						gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+						marginTop: "2rem",
+						display: "flex",
+						flexDirection: "column",
 						gap: "20px"
 					}}
 				>
 					<Post
 						title="titulo"
 						date="data"
-						summary="resumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumoresumo"
+						summary=" resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo resumo"
 					/>
 					<Post title="titulo" date="data" summary="resumo" />
 					<Post title="titulo" date="data" summary="resumo" />
